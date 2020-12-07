8e40ed25d98dcff6be2b0f7c7c6eec8684c9e57b
ALINA991 <53019682+ALINA991@users.noreply.github.com>
2020-12-07 17:01:09 -0500

    Update solitaire.py

--- solitaire.py
+++ solitaire.py
@@ -2,6 +2,38 @@
 # tel qu'indiqué dans la description du TP2.  Le code ici correspond
 # à l'exemple donné dans la description.
 
+''' A faire : 
+- remplir init() avec case 4 à 52 : 
+        - info utile : si on a les cartes x et y, elles ont la mˆeme couleur si x%4 = y%4, et elles ont la mˆeme valeur si x//4 = y//4.
+        - trouver nom de cartes dans dossier 'cards'
+        - faire avec BOUCLES (voir descitption du tp ) 
+        - voir demo ou on a créer un tableau en prog web 
+        - faire en sorte de les mélanger aléatirement au chargement (random()? ) 
+        
+- lister document.querySelector("#casen") avec chaque numero de carte -> permet d'Acceder a la carte 
+
+- creer fonction clic() qui definir action a faire lorsque clic : 
+        - déplacement au prochain emplacement possible :
+                - 2 seulement tout a droite 
+                - toute autre carte juste a droit de valeur just en dessous & meme signe 
+                
+- creer fonction deplacementPossible() pour vois si on peu cliquer sur la carte --> si oui mettre fond vert sur carte 
+        - utiliser #carteN comme identificateur -> remplir innerHTML
+        
+- creer fonction brasser carte() 
+        - avoir compteur pour brasser au max 3 fois 
+        - brasser seulement cartes qui ne sont pas encore aligne a gauche a partir de 2
+        - creer bouton 'brasser les cartes' et afficher a lecran du joueur
+        
+- creer pourton nouvelle partie qui apelle init() = nouvelle partie 
+
+- fonction terminé jeu qui se fait apeller si : 
+        - toute les cartes sont au bon endroit ( verification dans fonction deplacement possible? boucle while true? ) : 
+                - afficher victoire
+        - le joueur ne peux plus brasser (compteur = 0 ) ET il ne peux plus deplacer de cartes 
+                - afficher perdu 
+           
+
 def init():
     main = document.querySelector("#main")
     main.innerHTML = """
